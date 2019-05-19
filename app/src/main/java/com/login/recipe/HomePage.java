8597211c968fcd92584cd825e313d81b9972b74d
Aharon Louzon <aharon@dataloop.ai>
2019-05-19 18:29:16 +0300

    initial commit

---
+++ app/src/main/java/com/login/recipe/HomePage.java
@@ -0,0 +1,45 @@
+package com.login.recipe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomePage extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page2);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.user_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.logout_button: {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(HomePage.this, MainActivity.class));
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/login/recipe/MainActivity.java
@@ -0,0 +1,77 @@
+package com.login.recipe;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private Button register;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email = (EditText)findViewById(R.id.email_input);
+        password = (EditText)findViewById(R.id.password_input);
+        login = (Button)findViewById(R.id.login_button);
+        register = (Button)findViewById(R.id.forgot_button);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        progressDialog = new ProgressDialog(this);
+
+        if(user != null){
+            finish();
+            startActivity(new Intent(MainActivity.this, HomePage.class));
+        }
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegiterPage.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressDialog.setMessage("Cooking...");
+                progressDialog.show();
+                validate(email.getText().toString(), password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String email, String password){
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                    startActivity(new Intent(MainActivity.this, HomePage.class));
+                else
+                    Toast.makeText(MainActivity.this, "Login failed", Toast.LENGTH_SHORT);
+
+            }
+        });
+    }
+}
