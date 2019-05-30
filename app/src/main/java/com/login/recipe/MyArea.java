630bf83b19116b1429dd80d49b4e25f8474f9662
Aharon Louzon <aharon@dataloop.ai>
2019-05-31 01:01:30 +0300

    eod

--- app/src/main/java/com/login/recipe/MyArea.java
+++ app/src/main/java/com/login/recipe/MyArea.java
@@ -7,10 +7,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MyArea extends AppCompatActivity {
 
     private FloatingActionButton floatingActionButton;
+    private ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,10 @@ public class MyArea extends AppCompatActivity {
         setContentView(R.layout.activity_my_area);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+//        avatar = (ImageView)findViewById(R.id.imageView_my_area);
+
+//        avatar.setImageDrawable(R.drawable.male_avatar.png);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
