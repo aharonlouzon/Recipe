2eecf59f662a360368d60cc782dd33a920f6f0d3
dapfel <dapfel10@gmail.com>
2019-09-13 12:35:50 +0300

    added updateUserProfile method

--- app/src/main/java/com/login/recipe/DatabaseServiceTask.java
+++ app/src/main/java/com/login/recipe/DatabaseServiceTask.java
@@ -39,6 +39,11 @@ public class DatabaseServiceTask extends AsyncTask <Object, Integer , Object> {
                     // objects[0] is email. objects[1] is new pic (a byte[]).
                     result = database.changeProfilePic((byte[]) objects[0], (String) objects[1]);
                     break;
+                case "updateUserProfile":
+                    // objects[0] is email. objects[1] is new password. objects[2] is newFirstName. objects[3] is newLastName.
+                    // objects[4] is newCountry. objects[5] is newCuisines (array). objects[6] is newSkillLevel (of type skilllevel).
+                    result = database.updateUserProfileString((String) objects[0], (String) objects[1], (String) objects[2],
+                                                              (String) objects[3], (String) objects[4], (String[]) objects[5], (UserProfile.skillLevel) objects[6]);
                 case "addFollower":
                     // objects[0] is followedEmail. objects[1] is follower email.
                     result = database.addFollower((String) objects[0], (String) objects[1]);
